1be99f2d20698c21797ef3bd181cb081df61bcd0
Daksh Desai <dakshdesai54@gmail.com>
2025-04-05 02:03:24 +0530

    Prepared temporary schema

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,55 @@
 const functions = require("firebase-functions/v1");
 const admin = require("firebase-admin");
-const crypto = require("crypto");
+
 
 admin.initializeApp();
 
 exports.newUser = functions.auth.user().onCreate(async (user) => {
   if (user.email?.endsWith("@iitgn.ac.in")) {
+    var officialEventsEmailList = ['blithchron@iitgn.ac.in','amalthea@iitgn.ac.in','metis@iitgn.ac.in','24110098@iitgn.ac.in']; // add more emails here for events
+    var officialClubsEmailList = ['metis@iitgn.ac.in'] // add more clubs here 
+    var officialEmails = ['sports@iitgn.ac.in','acad@iitgn.ac.in'] // add more emails here for official emails
+    
+    if (officialEventsEmailList.includes(user.email)) {
+      admin.firestore().collection("users").doc(user.uid).set({
+        email: user.email,
+        displayName: user.displayName,
+        uid: user.uid,
+        roles: [user.email.split("@")[0]], // event
+        clubAffiliation: [], // subscriptions to clubs
+        intrests: [], // intrests of the user if using that
+      });
+    }
+    if (officialClubsEmailList.includes(user.email)) {
+      admin
+        .firestore()
+        .collection("users")
+        .doc(user.uid)
+        .set({
+          email: user.email,
+          displayName: user.displayName,
+          uid: user.uid,
+          roles: [user.email.split("@")[0]], // club
+          clubAffiliation: [], // subscriptions to other clubs
+          intrests: [], // intrests of the user if using that
+        });
+    }
+    if (officialEmails.includes(user.email)) {
+      admin
+        .firestore()
+        .collection("users")
+        .doc(user.uid)
+        .set({
+          email: user.email,
+          displayName: user.displayName,
+          uid: user.uid,
+          roles: [user.email.split("@")[0]], // official emails
+          clubAffiliation: [], // subscriptions to clubs
+          intrests: [], // intrests of the user if using that
+        });
+    }
+
+
     admin.firestore().collection("users").doc(user.uid).set({
       email: user.email,
       displayName: user.displayName,
@@ -13,9 +57,46 @@ exports.newUser = functions.auth.user().onCreate(async (user) => {
       roles: [], // student, admin, club-cordi, club-member, club-generalmember, etc.
       clubAffiliation: [], // subscriptions to clubs
       intrests: [], // intrests of the user if using that
+      participation: [], // events participating in
     });
   } else {
     await admin.auth().deleteUser(user.uid);
     console.log(`Deleted user ${user.uid} with email ${user.email}`);
   }
 });
+
+
+
+
+// event database schema
+{
+    // eventsCollection 
+    // {
+    //   eventId: "event id",
+    //   title: "event name",
+    //   description: "event description",
+    //   date: "event date",
+    //   time: "event time",
+    //   location: "event location",
+    //   filters: ["tech", "cult"],
+    //   clubAffiliation: ["club1", "club2"],
+    //   participants: ["user1", "user2"],
+    //   isActive: true, 
+    //   poster: "poster url",
+    //   postedBy: "user id",
+    //   createdAt: "timestamp",
+    // }
+}
+
+// club database schema
+{
+    // clubsCollection 
+    // {
+    //   clubId: "club id",
+    //   name: "club name",
+    //   description: "club description",
+    //   members: ["user1", "user2"],
+    //   subscribers: 100,
+    //   clubPhoto: "club photo url",
+    // }
+}
